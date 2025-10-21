0b50157ff3eb05206f0e0d7e977c0dbd495a24f1
Santiago Pizarro <santiagopizarropizarro.guadalupe@alumnado.fundacionloyola.net>
2025-10-21 12:10:31 +0200

    mostrar acciones - incecesario

--- mostrar_regiones_acciones.php
+++ mostrar_regiones_acciones.php
@@ -6,19 +6,23 @@ require 'configDB.php';
 $sql = "SELECT codigo, nombre FROM regiones";
 $resultado = $conexion->query($sql);
 
-// Mostramos el contenido
-/* if ($resultado->num_rows > 0) {
-    while ($fila = $resultado->fetch_array()) {
-        foreach ($fila as $key => $value) {
-            echo "" . $key . " - " . $value . "</br></br>";
-        }
-    }
-} */
-
-// Mostrar el contenido
+// Mostrar el contenido de la tabla regiones
 if ($resultado->num_rows > 0) {
     while ($fila = $resultado->fetch_assoc()) {
-        echo "" . $fila['codigo'] . " - " . $fila['nombre'] . "</br></br>";
+        echo "" . $fila['codigo'] . " - " . $fila['nombre'] . "</br>";
+    }
+}
+
+echo "</br>";
+
+// Consulta a la tabla acciones
+$sql = "SELECT id_accion, descripcion FROM acciones";
+$resultado = $conexion->query($sql);
+
+// Mostrar el contenido de la tabla acciones
+if ($resultado->num_rows > 0) {
+    while ($fila = $resultado->fetch_assoc()) {
+        echo "" . $fila['id_accion'] . " - " . $fila['descripcion'] . "</br>";
     }
 }
 
